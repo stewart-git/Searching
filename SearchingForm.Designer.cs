4bed9b59715a66792673de5808569143818fdbbe
stewart-git <Stewart.Godwin@smtafe.wa.edu.au>
2022-04-06 12:19:00 +0800

    Start

---
+++ SearchingForm.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace Searching
+{
+    partial class SearchingForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ListViewColours = new System.Windows.Forms.ListView();
+            this.Colour = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ButonBinarySearch = new System.Windows.Forms.Button();
+            this.TextBoxTarget = new System.Windows.Forms.TextBox();
+            this.ButtonJumpSearch = new System.Windows.Forms.Button();
+            this.LabelSearchTarget = new System.Windows.Forms.Label();
+            this.ButtonLinearSeach = new System.Windows.Forms.Button();
+            this.ButtonTernarySearch = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ListViewColours
+            // 
+            this.ListViewColours.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Colour});
+            this.ListViewColours.HideSelection = false;
+            this.ListViewColours.Location = new System.Drawing.Point(12, 12);
+            this.ListViewColours.Name = "ListViewColours";
+            this.ListViewColours.Size = new System.Drawing.Size(121, 290);
+            this.ListViewColours.TabIndex = 0;
+            this.ListViewColours.UseCompatibleStateImageBehavior = false;
+            this.ListViewColours.View = System.Windows.Forms.View.Details;
+            // 
+            // Colour
+            // 
+            this.Colour.Text = "My Colours";
+            this.Colour.Width = 109;
+            // 
+            // ButonBinarySearch
+            // 
+            this.ButonBinarySearch.Location = new System.Drawing.Point(155, 65);
+            this.ButonBinarySearch.Name = "ButonBinarySearch";
+            this.ButonBinarySearch.Size = new System.Drawing.Size(100, 23);
+            this.ButonBinarySearch.TabIndex = 1;
+            this.ButonBinarySearch.Text = "Binary Search";
+            this.ButonBinarySearch.UseVisualStyleBackColor = true;
+            this.ButonBinarySearch.Click += new System.EventHandler(this.ButonBinarySearch_Click);
+            // 
+            // TextBoxTarget
+            // 
+            this.TextBoxTarget.Location = new System.Drawing.Point(155, 39);
+            this.TextBoxTarget.Name = "TextBoxTarget";
+            this.TextBoxTarget.Size = new System.Drawing.Size(100, 20);
+            this.TextBoxTarget.TabIndex = 2;
+            // 
+            // ButtonJumpSearch
+            // 
+            this.ButtonJumpSearch.Location = new System.Drawing.Point(155, 94);
+            this.ButtonJumpSearch.Name = "ButtonJumpSearch";
+            this.ButtonJumpSearch.Size = new System.Drawing.Size(100, 23);
+            this.ButtonJumpSearch.TabIndex = 3;
+            this.ButtonJumpSearch.Text = "Jump Search";
+            this.ButtonJumpSearch.UseVisualStyleBackColor = true;
+            this.ButtonJumpSearch.Click += new System.EventHandler(this.ButtonJumpSearch_Click);
+            // 
+            // LabelSearchTarget
+            // 
+            this.LabelSearchTarget.AutoSize = true;
+            this.LabelSearchTarget.Location = new System.Drawing.Point(152, 23);
+            this.LabelSearchTarget.Name = "LabelSearchTarget";
+            this.LabelSearchTarget.Size = new System.Drawing.Size(75, 13);
+            this.LabelSearchTarget.TabIndex = 4;
+            this.LabelSearchTarget.Text = "Search Target";
+            // 
+            // ButtonLinearSeach
+            // 
+            this.ButtonLinearSeach.Location = new System.Drawing.Point(155, 123);
+            this.ButtonLinearSeach.Name = "ButtonLinearSeach";
+            this.ButtonLinearSeach.Size = new System.Drawing.Size(100, 23);
+            this.ButtonLinearSeach.TabIndex = 5;
+            this.ButtonLinearSeach.Text = "Linear Search";
+            this.ButtonLinearSeach.UseVisualStyleBackColor = true;
+            this.ButtonLinearSeach.Click += new System.EventHandler(this.ButtonLinearSeach_Click);
+            // 
+            // ButtonTernarySearch
+            // 
+            this.ButtonTernarySearch.Location = new System.Drawing.Point(155, 152);
+            this.ButtonTernarySearch.Name = "ButtonTernarySearch";
+            this.ButtonTernarySearch.Size = new System.Drawing.Size(100, 23);
+            this.ButtonTernarySearch.TabIndex = 6;
+            this.ButtonTernarySearch.Text = "Ternary Search";
+            this.ButtonTernarySearch.UseVisualStyleBackColor = true;
+            this.ButtonTernarySearch.Click += new System.EventHandler(this.ButtonTernarySearch_Click);
+            // 
+            // SearchingForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(300, 337);
+            this.Controls.Add(this.ButtonTernarySearch);
+            this.Controls.Add(this.ButtonLinearSeach);
+            this.Controls.Add(this.LabelSearchTarget);
+            this.Controls.Add(this.ButtonJumpSearch);
+            this.Controls.Add(this.TextBoxTarget);
+            this.Controls.Add(this.ButonBinarySearch);
+            this.Controls.Add(this.ListViewColours);
+            this.Name = "SearchingForm";
+            this.Text = "My Searching Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView ListViewColours;
+        private System.Windows.Forms.Button ButonBinarySearch;
+        private System.Windows.Forms.TextBox TextBoxTarget;
+        private System.Windows.Forms.ColumnHeader Colour;
+        private System.Windows.Forms.Button ButtonJumpSearch;
+        private System.Windows.Forms.Label LabelSearchTarget;
+        private System.Windows.Forms.Button ButtonLinearSeach;
+        private System.Windows.Forms.Button ButtonTernarySearch;
+    }
+}
+
---
+++ SearchingForm.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Searching
+{
+    public partial class SearchingForm : Form
+    {
+        public SearchingForm()
+        {
+            InitializeComponent();
+            DisplayRainbow();
+        }
+        string[] Rainbow = new string[] {"Amber","Blue","Crimson","Ebony","Green","Indego",
+            "Lavender","Maroon","Orange","Purle","Red","Violet","Yellow"};
+        // Display the array of string vlaues
+        private void DisplayRainbow()
+        {
+            ListViewColours.Items.Clear();
+            for(int x = 0; x < Rainbow.Length; x++)
+            {
+                ListViewItem lvi = new ListViewItem(Rainbow[x]);
+                lvi.SubItems.Add(Rainbow[x].ToString());
+                ListViewColours.Items.Add(lvi);
+            }
+        }
+        // Binary Search. In this procedure, the entire list is divided into two sub-lists. 
+        // If the item is found in the middle position, it returns the location, otherwise 
+        // jumps to either left or right sub-list and do the same process again until finding 
+        // the item or exceed the range.
+        private void ButonBinarySearch_Click(object sender, EventArgs e)
+        {
+            if(!string.IsNullOrEmpty(TextBoxTarget.Text))
+            {
+                bool found = false; // assume the item has not been found
+                int min = 0;              
+                int max = Rainbow.Length - 1; // max is one less than size
+                string findThis = TextBoxTarget.Text;
+                while (min <= max)
+                {
+                    int mid = ((min + max) / 2); // uses integer division
+                    if(findThis.CompareTo(Rainbow[mid]) == 0)
+                    {
+                        found = true;
+                        break;
+                    }
+                    else if(findThis.CompareTo(Rainbow[mid]) < 0)
+                    {
+                        max = mid - 1;
+                    }
+                    else
+                    {
+                        min = mid + 1;
+                    }
+
+                }
+                if(!found)
+                {
+                    MessageBox.Show("Search failed: item not found");
+                }
+                else
+                {
+                    MessageBox.Show("Success: the item was found");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter a colour into the TextBox");
+            }
+            TextBoxTarget.Clear();
+        }
+        // Jump Search. Creates a block and tries to find the element in that block. 
+        // If the item is not in the block, it shifts the entire block. The block 
+        // size is based on the size of the list. If the size of the list is n then 
+        // block size will be √n. After finding a correct block it finds the item 
+        // using a linear search technique. The jump search lies between linear search 
+        // and binary search according to its performance.
+        private void ButtonJumpSearch_Click(object sender, EventArgs e) 
+        {
+            bool found = false; // assume the item has not been found
+            string target = TextBoxTarget.Text;
+            int max = Rainbow.Length;
+            int step = (int)Math.Floor(Math.Sqrt(max)); // block size
+            int prev = 0;
+            while(string.Compare((Rainbow[Math.Min(step, max) - 1]), target) == -1)
+            {
+                prev = step;
+                step += (int)Math.Floor(Math.Sqrt(max));
+                if (prev >= max)
+                {
+                    found = false;
+                    break;
+                }                   
+            }
+            while(string.Compare(Rainbow[prev], target) == -1)
+            {
+                prev++;
+                if (prev == Math.Min(step, max))
+                {
+                    found = false;
+                    break;
+                }
+            }
+            if (string.Compare(Rainbow[prev], target) == 0)
+                found = true;
+            if (!found)
+            {
+                MessageBox.Show("Search failed: item not found");
+            }
+            else
+            {
+                MessageBox.Show("Success: the item was found");
+            }
+            TextBoxTarget.Clear();
+        }
+        // linear Search
+        private void ButtonLinearSeach_Click(object sender, EventArgs e)
+        {
+            bool found = false; // assume the item has not been found
+            string target = TextBoxTarget.Text;
+            // linear Search code
+            for (int x = 0; x < Rainbow.Length; x++)
+            {
+                if(string.Compare(Rainbow[x], target) == 0)
+                {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found)
+            {
+                MessageBox.Show("Search failed: item not found");
+            }
+            else
+            {
+                MessageBox.Show("Success: the item was found");
+            }
+            TextBoxTarget.Clear();
+        }
+        // 
+        private void ButtonTernarySearch_Click(object sender, EventArgs e)
+        {
+            bool found = false; // assume the item has not been found
+            string target = TextBoxTarget.Text;
+            int lo = 0;
+            int hi = Rainbow.Length - 1;
+
+            while (hi >= lo)
+            {
+                int mid1 = lo + (hi - lo) / 3;
+                int mid2 = hi - (hi - lo) / 3;
+
+                if (string.Compare(Rainbow[mid1], target) == 0)
+                {
+                    found = true;
+                    break;
+                }
+                if (string.Compare(Rainbow[mid2], target) == 0)
+                {
+                    found = true;
+                    break;
+                }
+                if (target.CompareTo(Rainbow[mid1]) < 0)
+                {
+                    hi = mid1 - 1;
+                }           
+                else if (target.CompareTo(Rainbow[mid2]) > 0)
+                {
+                    lo = mid2 + 1;
+                }
+                else
+                {
+                    lo = mid1 + 1;
+                    hi = mid2 - 1;
+                }
+            }
+
+            if (!found)
+            {
+                MessageBox.Show("Search failed: item not found");
+            }
+            else
+            {
+                MessageBox.Show("Success: the item was found");
+            }
+            TextBoxTarget.Clear();
+        }
+    }
+}
